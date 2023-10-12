8fd0bbe9e6e87237af5c413688008a25fa326b96
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-10-12 23:15:51 +0200

    Free + Card Component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import superEth from '../assets/supereth.png';
 
-const Card = () => {
-    return <div>Card</div>;
+const Card = ({ image, series, title, price, tag, time }) => {
+    return (
+        <div className="card">
+            <div className="card-image">
+                <img src={image} alt="superEth" />
+            </div>
+
+            <div className="card-content">
+                <div className="card-heading">
+                    <span className="card-series">{series}</span>
+                    <span className="card-top">Top bid</span>
+                </div>
+                <div className="card-details">
+                    <h4 className="card-title">{title}</h4>
+                    <div className="card-price">
+                        <img src={superEth} alt="superEth" />
+                        <h4>{price} ETH</h4>
+                    </div>
+                </div>
+
+                <div className="card-sub-details">
+                    <span>#{tag}</span>
+                    <span>{time} day left</span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Card;
