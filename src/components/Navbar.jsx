ee38e41ee3ede60f1fc3cc0858820707b830faf0
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-10-12 17:58:37 +0200

    Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GiHamburgerMenub } from 'react-icons/gi';
+import { MdClose } from 'react-icons/md';
+import { ImSun } from 'react-icons/im';
+import { BsFillMoonFill } from 'react-icons/bs';
+import logo from '../assets/logo.png';
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    const [navState, useNavState] = useState(false);
+
+    return (
+        <nav>
+            <div className="brand-container">
+                <div className="brand">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="toggle-container">
+                    <div className="toggle"></div>
+                    <div className="mode"></div>
+                </div>
+            </div>
+            <div className="links-container">
+                <ul className="links">
+                    <li>
+                        <a href="#">Features</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Launch</a>
+                    </li>
+                    <li>
+                        <a href="#">Sing up</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
