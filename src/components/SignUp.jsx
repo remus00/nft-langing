21c3d98aab044f64ab58533cf6c89cf617f06686
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-10-13 10:13:10 +0200

    SignUp component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
-
+import signup from '../assets/signup.png';
 const SignUp = () => {
-    return <div>SignUp</div>;
+    return (
+        <div className="signup">
+            <div className="container">
+                <div className="content">
+                    <p className="subtitle">Launching Soon</p>
+                    <h1 className="title">An NFT like no other</h1>
+                    <p className="description">
+                        Don't miss out on the release of our new NFT. Sign up below to receive
+                        updates when we go live.
+                    </p>
+                    <button>Sign up</button>
+                </div>
+                <div className="image-container">
+                    <div className="image">
+                        <img src={signup} alt="home" />
+                    </div>
+                    <div className="ellipse-container">
+                        <div className="ellipse pink"></div>
+                        <div className="ellipse orange"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default SignUp;
